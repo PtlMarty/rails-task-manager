eca2b16269d9098c37754ee0a9afb4e19600e5cb
portal martin <ptl_martin@yahoo.co.jp>
2024-01-03 16:24:32 +0900

    bordel, cest dur

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,20 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to "/", notice: 'Task was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
 end
